1ee4838e581acb12f7b508e780f35b39c574b3ae
Zeeshan Shrestha <shresthajeesan@gmail.com>
2025-07-05 16:01:25 +0545

    added chapter 1 and chaper 2

---
+++ Chapter2(Variables).py
@@ -0,0 +1,11 @@
+# A variable is a container for a data 
+#and that data can be anything word,number,boolean,object,double
+
+a = 2 #number
+b = "Nara" #word or string
+c = True #boolean True Or False
+d = 2.2 #double decimal
+print(a)
+print(b)
+print(c)
+print(d)
