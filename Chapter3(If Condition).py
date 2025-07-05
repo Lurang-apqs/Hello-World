2fcf30cc13c7a24040e2da4bcc7dd463c5efdbd9
Zeeshan Shrestha <shresthajeesan@gmail.com>
2025-07-05 16:09:38 +0545

    added if  statement

---
+++ Chapter3(If Condition).py
@@ -0,0 +1,10 @@
+#if statement
+
+playerHealth= 100
+swordDamage = 10
+print(playerHealth)
+#assume swordhastouched the player
+if True:
+    playerHealth = playerHealth -swordDamage
+
+print(playerHealth)
