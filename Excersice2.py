2a348c628610ecd512dd0695fd8a2d75363dfdb1
Zeeshan Shrestha <shresthajeesan@gmail.com>
2025-07-05 17:50:38 +0545

    added big projects

---
+++ Excersice2.py
@@ -0,0 +1,8 @@
+#WAP to make a list of foods and sort them in reverse alphabetical order
+
+#Wap to make a string and capitalize all the words and print it in the terminal
+
+# BIG PROJECT 
+# WAP to make a quiz game with 10 questions than checks if the question answer 
+# given by the user is correct or not and also print the result on the terminal at
+# last Hint: this is how u get user input ->  code : input("")
